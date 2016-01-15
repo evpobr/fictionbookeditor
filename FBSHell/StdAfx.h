c5c9c98bd4a37d915c24ce0ede627ae74eb8850c
evpobr <evpobr@gmail.com>
2016-01-15 12:04:13 +0500

    Move SDK version defines to targetver.h, cleanup stdafx.h

--- FBSHell/StdAfx.h
+++ FBSHell/StdAfx.h
@@ -1,23 +1,11 @@
-// stdafx.h : include file for standard system include files,
-//  or project specific include files that are used frequently, but
-//      are changed infrequently
-//
-
-#if !defined(AFX_STDAFX_H__AA787312_D02C_4332_A4DD_1B1AA8B9E8BF__INCLUDED_)
-#define AFX_STDAFX_H__AA787312_D02C_4332_A4DD_1B1AA8B9E8BF__INCLUDED_
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
+
+#include "targetver.h"
 
 // check for unicode
 #ifndef UNICODE
 #error This program requires unicode support to run
 #endif
-
-#define WINVER       0x0501     // WinXP
-#define _WIN32_WINNT 0x0501     // WinXP
-#define _WIN32_IE    0x0600     // IE 6+
 
 // we are MT by default
 #define _ATL_MULTI_THREADED
@@ -61,7 +49,3 @@ extern CComModule _Module;
 
 using namespace _com_util;
 
-//{{AFX_INSERT_LOCATION}}
-// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
-
-#endif // !defined(AFX_STDAFX_H__AA787312_D02C_4332_A4DD_1B1AA8B9E8BF__INCLUDED_)
