e11b2d5653ddcbeacefa8b251cebb6c7cb81dc80
evpobr <evpobr@gmail.com>
2016-01-20 12:11:16 +0500

    Rewrite FBShell with GDI+ part 2

---
+++ FBSHell/dllmain.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class CFBShellModule : public ATL::CAtlDllModuleT< CFBShellModule >
+{
+public:
+	DECLARE_LIBID(LIBID_FBShellLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_FBSHELL, "{BE36E0D7-CE35-4586-A3F7-5883E21A9A34}")
+};
+
+extern class CFBShellModule _AtlModule;
